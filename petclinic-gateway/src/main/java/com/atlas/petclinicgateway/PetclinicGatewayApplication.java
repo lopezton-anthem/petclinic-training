39a4c47e1fa3c578275f455e833b2d6868b8eaff
Tony Lopez <antoniotony.lopez@anthem.com>
2019-01-03 09:13:16 -0600

    WIP - OAuth Changes

--- petclinic-gateway/src/main/java/com/atlas/petclinicgateway/PetclinicGatewayApplication.java
+++ petclinic-gateway/src/main/java/com/atlas/petclinicgateway/PetclinicGatewayApplication.java
@@ -2,6 +2,7 @@ package com.atlas.petclinicgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 public class PetclinicGatewayApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PetclinicGatewayApplication.class, args);
 	}
