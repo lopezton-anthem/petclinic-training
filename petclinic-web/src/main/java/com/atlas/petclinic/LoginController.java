2c0c456d3cdc7821dfcb14c77e6cc3d5e6f9c16c
lopezton-anthem <antoniotony.lopez@anthem.com>
2019-06-13 13:48:02 -0500

    blah

--- petclinic-web/src/main/java/com/atlas/petclinic/LoginController.java
+++ petclinic-web/src/main/java/com/atlas/petclinic/LoginController.java
@@ -5,13 +5,17 @@ package com.atlas.petclinic;
 
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 /**
@@ -21,10 +25,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
+	@Autowired
+	private ObjectMapper om;
+	
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public void login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	response.addCookie(new Cookie("NIMBUS_TENANT_ID", "/anthem/nimbus/petclinic"));
     	response.sendRedirect("/petclinic/#/h/petclinicdashboard/vpDashboard");
-        
     }
     
 
